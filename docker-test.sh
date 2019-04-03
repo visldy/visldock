94df65e9a9de99cfa1114d42f81160dab3cf72ed
Daniel Yagodin <dy@technion.ac.il>
2019-04-03 13:29:33 +0300

    first commit

--- docker-test.sh
+++ docker-test.sh
@@ -1,3 +1,3 @@
 # Test Dicker with CUDA 10
-docker run --runtime=nvidia --rm nvidia/cuda:10.0-base-ubuntu18.04 nvidia-smi
+docker run --runtime=nvidia --rm nvidia/cuda:10.1-base-ubuntu18.04 nvidia-smi
 
